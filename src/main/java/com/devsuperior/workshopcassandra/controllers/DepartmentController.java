a53f977b36c4e77670b0557c7983e8d103c77fa6
Henrique Marques <132225341+henriquecesarjr@users.noreply.github.com>
2024-10-11 10:10:43 -0300

    GET all departments

---
+++ src/main/java/com/devsuperior/workshopcassandra/controllers/DepartmentController.java
@@ -0,0 +1,27 @@
+package com.devsuperior.workshopcassandra.controllers;
+
+import com.devsuperior.workshopcassandra.model.dto.DepartmentDTO;
+import com.devsuperior.workshopcassandra.services.DepartmentService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/departments")
+public class DepartmentController {
+
+    private final DepartmentService departmentService;
+
+    public DepartmentController(DepartmentService departmentService) {
+        this.departmentService = departmentService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<DepartmentDTO>> findAll() {
+        List<DepartmentDTO> list = departmentService.findAll();
+        return ResponseEntity.ok(list);
+    }
+}
---
+++ src/main/java/com/devsuperior/workshopcassandra/services/DepartmentService.java
@@ -0,0 +1,25 @@
+package com.devsuperior.workshopcassandra.services;
+
+import com.devsuperior.workshopcassandra.model.dto.DepartmentDTO;
+import com.devsuperior.workshopcassandra.model.entities.Department;
+import com.devsuperior.workshopcassandra.repositories.DepartmentRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class DepartmentService {
+
+    private final DepartmentRepository departmentRepository;
+
+    public DepartmentService(DepartmentRepository departmentRepository) {
+        this.departmentRepository = departmentRepository;
+    }
+
+    public List<DepartmentDTO> findAll() {
+        List<Department> list = departmentRepository.findAll();
+        return list.stream().map(DepartmentDTO::new).collect(Collectors.toList());
+    }
+
+}
