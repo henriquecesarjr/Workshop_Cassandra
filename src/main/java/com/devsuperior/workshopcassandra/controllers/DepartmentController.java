9bb1b77755f5807ff03473108f362752b23f9830
Henrique Marques <132225341+henriquecesarjr@users.noreply.github.com>
2024-10-11 10:22:39 -0300

    GET Department by id

--- src/main/java/com/devsuperior/workshopcassandra/controllers/DepartmentController.java
+++ src/main/java/com/devsuperior/workshopcassandra/controllers/DepartmentController.java
@@ -4,10 +4,12 @@ import com.devsuperior.workshopcassandra.model.dto.DepartmentDTO;
 import com.devsuperior.workshopcassandra.services.DepartmentService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping(value = "/departments")
@@ -23,5 +25,11 @@ public class DepartmentController {
     public ResponseEntity<List<DepartmentDTO>> findAll() {
         List<DepartmentDTO> list = departmentService.findAll();
         return ResponseEntity.ok(list);
+    }
+
+    @GetMapping(value = "{id}")
+    public ResponseEntity<DepartmentDTO> findById(@PathVariable UUID id) {
+        DepartmentDTO obj = departmentService.findById(id);
+        return ResponseEntity.ok(obj);
     }
 }
--- src/main/java/com/devsuperior/workshopcassandra/services/DepartmentService.java
+++ src/main/java/com/devsuperior/workshopcassandra/services/DepartmentService.java
@@ -3,9 +3,11 @@ package com.devsuperior.workshopcassandra.services;
 import com.devsuperior.workshopcassandra.model.dto.DepartmentDTO;
 import com.devsuperior.workshopcassandra.model.entities.Department;
 import com.devsuperior.workshopcassandra.repositories.DepartmentRepository;
+import com.devsuperior.workshopcassandra.services.exceptions.ResourceNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 @Service
@@ -20,6 +22,12 @@ public class DepartmentService {
     public List<DepartmentDTO> findAll() {
         List<Department> list = departmentRepository.findAll();
         return list.stream().map(DepartmentDTO::new).collect(Collectors.toList());
+    }
+
+    public DepartmentDTO findById(UUID id) {
+        Department entity = departmentRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Resource not found"));
+        return new DepartmentDTO(entity);
     }
 
 }
